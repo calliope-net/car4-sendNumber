ea0f2e1edb72eeedf4c14df0cdd25163d45f9393
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-02-02 20:49:53 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -16,7 +16,12 @@ loops.everyInterval(400, function () {
         iMotor = aJoy[0]
         qwiicjoystick.comment("0 Motor 0..128..255")
         iServo = aJoy[1]
-    } else {
-    	
+        iServo = Math.round(Math.map(iServo, 0, 255, 135, 45))
+        qwiicjoystick.comment("1 Servo 0..128..255 -> 45..90..135")
+        qwiicjoystick.setSendeZahl(NumberFormat.Int8LE, qwiicjoystick.eOffset.z0, iMotor)
+        qwiicjoystick.setSendeZahl(NumberFormat.Int8LE, qwiicjoystick.eOffset.z1, iServo)
+        qwiicjoystick.setSendeZahl(NumberFormat.Int8LE, qwiicjoystick.eOffset.z2, 0)
+        radio.sendNumber(qwiicjoystick.getSendeZahl())
+        basic.turnRgbLedOff()
     }
 })
