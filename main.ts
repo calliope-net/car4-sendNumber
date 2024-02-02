21f32de21c5716051b80cd00ceb5351339b4151a
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-02-02 20:41:33 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+let aJoy: number[] = []
+let iFahrstrecke = 0
 basic.forever(function () {
-	
+    qwiicjoystick.beimStart(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20))
+    aJoy = qwiicjoystick.readArray(qwiicjoystick.qwiicjoystick_eADDR(qwiicjoystick.eADDR.Joystick_x20), qwiicjoystick.eBereich.B_0_255)
+    radio.setGroup(240)
+    radio.setTransmitPower(7)
+    iFahrstrecke = 0
 })
